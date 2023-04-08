77e43727d6fb32052af26e70fdd1436f5b51c1ce
Jack Milner <jackmilner@gmail.com>
2023-04-08 06:32:05 +0100

    Refactor

--- main.go
+++ main.go
@@ -1,78 +1,97 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
 	"os"
-    "fmt"
-    "net/http"
-    "io/ioutil"
-	"bytes"
+
 	"encoding/json"
 )
 
 type Body struct {
-	Model string `json:"model"`
-	Messages []Messages `json:"messages"`
+	Model    string    `json:"model"`
+	Messages []Message `json:"messages"`
 }
 
-type Messages struct {
-	Role string `json:"role"`
+type Message struct {
+	Role    string `json:"role"`
 	Content string `json:"content"`
 }
 
+type Response struct {
+	Id      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int      `json:"created"`
+	Choices []Choice `json:"choices"`
+	Usage   Usage    `json:"usage"`
+}
+
+type Choice struct {
+	Index        int     `json:"index"`
+	Message      Message `json:"message"`
+	FinishReason string  `json:"finish_reason"`
+}
+
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
 func handleMessage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request!")
 
-	body := Body{
-		Model: "gpt-3.5-turbo",
-		Messages: []Messages{
-			{
-				Role: "user",
-				Content: "Hello",
-			},
-		},
-	}
-
-	jsonBody, err := json.Marshal(body)
+	var requestBody Body
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	bodyReader := bytes.NewReader(jsonBody)
 
+	fmt.Println(requestBody.Messages[0].Content)
+}
+
+func main() {
+	fmt.Println("Starting server...")
+	http.HandleFunc("/", handleMessage)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func requestChatGPT(bodyReader io.Reader) error {
 	client := &http.Client{}
 
-    // Send JSON payload to ChatGPT
+	// Send JSON payload to ChatGPT
 	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bodyReader)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	apiKey, err := os.ReadFile("api-key.txt")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return err
 	}
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
 
 	resp, err := client.Do(req)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-    // Read response from ChatGPT and write it back to the HTTP response writer
-    response, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    fmt.Fprintf(w, string(response))
-}
+	// Read response from ChatGPT and write it back to the HTTP response writer
+	var respStruct Response
+	err = json.NewDecoder(resp.Body).Decode(&respStruct)
+	if err != nil {
+		return err
+	}
+	fmt.Println(respStruct.Choices[0].Message.Content)
 
-func main() {
-    http.HandleFunc("/", handleMessage)
-    http.ListenAndServe(":8080", nil)
+	return nil
 }
